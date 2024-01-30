62cdfe73f8841442ac7926e1a097c6e829c8566a
chriswoodcn <chriswoodcn@aliyun.com>
2024-01-30 11:08:38 +0800

    [update] switch @AutoConfiguration

--- anthill-infrastructure/anthill-infrastructure-datasource/src/main/java/cn/chriswood/anthill/infrastructure/datasource/DataSourceAutoConfig.kt
+++ anthill-infrastructure/anthill-infrastructure-datasource/src/main/java/cn/chriswood/anthill/infrastructure/datasource/DataSourceAutoConfig.kt
@@ -1,12 +1,10 @@
 package cn.chriswood.anthill.infrastructure.datasource
 
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.validation.annotation.Validated
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnProperty(
     prefix = "anthill.datasource",
     name = ["enabled"],
--- anthill-infrastructure/anthill-infrastructure-json/src/main/java/cn/chriswood/anthill/infrastructure/json/JacksonConfig.kt
+++ anthill-infrastructure/anthill-infrastructure-json/src/main/java/cn/chriswood/anthill/infrastructure/json/JacksonConfig.kt
@@ -1,13 +1,14 @@
 package cn.chriswood.anthill.infrastructure.json
 
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnProperty(
     prefix = "anthill.jackson",
     name = ["enabled"],
