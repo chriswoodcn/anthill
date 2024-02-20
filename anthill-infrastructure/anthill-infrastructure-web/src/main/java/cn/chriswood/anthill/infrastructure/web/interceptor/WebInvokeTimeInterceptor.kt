9e04dd0ea70a49a16c3f3b029f00e9e87b2fa6c8
chriswoodcn <chriswoodcn@aliyun.com>
2024-02-20 08:29:54 +0800

    [update] 增加 判断是否是需要过滤的请求

--- anthill-infrastructure/anthill-infrastructure-web/src/main/java/cn/chriswood/anthill/infrastructure/web/interceptor/WebInvokeTimeInterceptor.kt
+++ anthill-infrastructure/anthill-infrastructure-web/src/main/java/cn/chriswood/anthill/infrastructure/web/interceptor/WebInvokeTimeInterceptor.kt
@@ -4,6 +4,7 @@ import cn.chriswood.anthill.infrastructure.json.JacksonUtil
 import cn.chriswood.anthill.infrastructure.core.utils.ObjectUtil
 import cn.hutool.core.io.IoUtil
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.ui.ModelMap
 import org.springframework.web.context.request.ServletWebRequest
@@ -15,16 +16,23 @@ class WebInvokeTimeInterceptor : WebRequestInterceptor {
     private val log = LoggerFactory.getLogger(WebInvokeTimeInterceptor::class.java)
     override fun preHandle(request: WebRequest) {
         if (request is ServletWebRequest) {
+            val isNeedFilterRequest = getIsNeedFilterRequest(request)
+            if (isNeedFilterRequest) return
             val r0 = request.request
-            if (r0 !is RepeatedlyRequestWrapper) {
-                return
-            }
-            r0.setAttribute("startTime", System.currentTimeMillis())
-            val jsonParam: Any?
-            jsonParam = if (!r0.contentType.isNullOrEmpty() && r0.contentType.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
-                IoUtil.read(r0.reader)
-            } else {
-                JacksonUtil.bean2string(r0.parameterMap)
+            val isRepeatedlyRequestWrapper = r0 !is RepeatedlyRequestWrapper
+            r0.setAttribute("anthill_web_invoke_start_time", System.currentTimeMillis())
+            var jsonParam: Any? = null
+            if (isRepeatedlyRequestWrapper) {
+                jsonParam =
+                    if (!r0.contentType.isNullOrEmpty() && r0.contentType.startsWith(MediaType.APPLICATION_JSON_VALUE)) {
+                        IoUtil.read(r0.reader)
+                    } else {
+                        if (r0.parameterMap.isNotEmpty()) {
+                            JacksonUtil.bean2string(r0.parameterMap)
+                        } else {
+                            null
+                        }
+                    }
             }
             if (ObjectUtil.isNull(jsonParam)) {
                 log.info(
@@ -44,17 +52,33 @@ class WebInvokeTimeInterceptor : WebRequestInterceptor {
         }
     }
 
+    /**
+     * 判断是否是需要过滤的请求
+     *
+     * HEAD请求、swagger相关请求
+     */
+    private fun getIsNeedFilterRequest(request: ServletWebRequest): Boolean {
+        if (request.httpMethod == HttpMethod.HEAD) return true
+        val requestURI = request.request.requestURI
+        val filters = arrayOf(
+            "/swagger-ui",
+            "/v3/api-docs"
+        )
+        return filters.any {
+            requestURI.startsWith(it)
+        }
+    }
+
     override fun postHandle(request: WebRequest, model: ModelMap?) {
 
     }
 
     override fun afterCompletion(request: WebRequest, ex: Exception?) {
         if (request is ServletWebRequest) {
+            val isNeedFilterRequest = getIsNeedFilterRequest(request)
+            if (isNeedFilterRequest) return
             val r0 = request.request
-            if (r0 !is RepeatedlyRequestWrapper) {
-                return
-            }
-            val startTime = r0.getAttribute("startTime") as Long
+            val startTime = r0.getAttribute("anthill_web_invoke_start_time") as Long
             val endTime = System.currentTimeMillis()
             log.info(
                 "结束请求 => [{}] URL[{}],耗时[{}ms]",
