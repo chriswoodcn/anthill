dd025574badbb3fc499ebc3309ed4d33f66a8db8
chriswoodcn <chriswoodcn@aliyun.com>
2024-02-28 11:02:26 +0800

    [fix] 加上@ReadingConverter

--- anthill-infrastructure/anthill-infrastructure-mongo/src/main/java/cn/chriswood/anthill/infrastructure/mongo/convert/Decimal128ToBigDecimalConvert.kt
+++ anthill-infrastructure/anthill-infrastructure-mongo/src/main/java/cn/chriswood/anthill/infrastructure/mongo/convert/Decimal128ToBigDecimalConvert.kt
@@ -2,8 +2,10 @@ package cn.chriswood.anthill.infrastructure.mongo.convert
 
 import org.bson.types.Decimal128
 import org.springframework.core.convert.converter.Converter
+import org.springframework.data.convert.ReadingConverter
 import java.math.BigDecimal
 
+@ReadingConverter
 class Decimal128ToBigDecimalConvert : Converter<Decimal128, BigDecimal> {
     override fun convert(source: Decimal128): BigDecimal {
         return source.bigDecimalValue()
