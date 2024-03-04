486253eea20b1b963158442864b8817290234bdf
chriswoodcn <chriswoodcn@aliyun.com>
2024-03-04 11:08:41 +0800

    [update] aliyun依赖版本管理

--- anthill-infrastructure/anthill-infrastructure-web-aliyun-oss-sts/build.gradle.kts
+++ anthill-infrastructure/anthill-infrastructure-web-aliyun-oss-sts/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
-    api("com.aliyun:aliyun-java-sdk-core:4.6.1")
-    api("com.aliyun:aliyun-java-sdk-sts:3.1.2")
+    api(libs.bundles.aliyun)
     compileOnly(project(mapOf("path" to ":anthill-infrastructure:anthill-infrastructure-core")))
     compileOnly(project(mapOf("path" to ":anthill-infrastructure:anthill-infrastructure-web")))
     compileOnly("org.springframework.boot:spring-boot-starter-web")
