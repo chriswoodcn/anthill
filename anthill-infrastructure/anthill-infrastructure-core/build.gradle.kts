ccd351c1684cd9673ad945b58c7d8f2886d8a054
chriswoodcn <chriswoodcn@aliyun.com>
2024-07-12 08:51:39 +0800

    [update] 1.2.7-SNAPSHOT BeanUtil使用kotlin kclass进行反射获取属性和设置属性

--- anthill-infrastructure/anthill-infrastructure-core/build.gradle.kts
+++ anthill-infrastructure/anthill-infrastructure-core/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     api(libs.hutool.all)
     implementation("org.apache.commons:commons-lang3")
     compileOnly("org.springframework.boot:spring-boot-starter-web")
+    compileOnly(kotlin("reflect"))
 }
--- anthill-infrastructure/anthill-infrastructure-core/src/main/java/cn/chriswood/anthill/infrastructure/core/utils/BeanUtil.kt
+++ anthill-infrastructure/anthill-infrastructure-core/src/main/java/cn/chriswood/anthill/infrastructure/core/utils/BeanUtil.kt
@@ -1,13 +1,32 @@
 package cn.chriswood.anthill.infrastructure.core.utils
 
-import org.springframework.beans.BeanUtils
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 
 object BeanUtil {
     inline fun <reified T : Any> copyBean(source: Any): T {
-        val mCreate = T::class.java.getDeclaredConstructor()
-        mCreate.isAccessible = true
-        val newInstance = mCreate.newInstance()
-        BeanUtils.copyProperties(source, newInstance)
-        return newInstance
+        val kClass = T::class
+        val declaredConstructor = kClass.java.getDeclaredConstructor()
+        declaredConstructor.isAccessible = true
+        val instance = kClass.objectInstance ?: declaredConstructor.newInstance()
+        val sourceFields = source::class.memberProperties
+        val targetFields = instance::class.memberProperties
+        val targetFieldInvokes: MutableMap<String, KMutableProperty1<out T, *>> = mutableMapOf()
+        targetFields.forEach {
+            it.isAccessible = true
+            targetFieldInvokes[it.name] = it as KMutableProperty1<out T, *>
+        }
+        if (targetFieldInvokes.isEmpty()) return instance
+        sourceFields.forEach() {
+            it.isAccessible = true
+            val fieldName = it.name
+            val fieldValue = it.getter.call(source)
+            if (fieldValue != null && targetFieldInvokes.keys.contains(fieldName)) {
+                val invoke = targetFieldInvokes[it.name]
+                invoke?.setter?.call(instance, fieldValue)
+            }
+        }
+        return instance
     }
 }
