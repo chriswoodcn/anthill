161add42dac2043571e6524d91a17e874489b406
chriswoodcn <chriswoodcn@aliyun.com>
2024-07-13 09:38:09 +0800

    [update] 优化OperateType枚举，StandardLogAppender只有在没有该类型bean情况下才会加载

--- anthill-infrastructure/anthill-infrastructure-web-annotation/src/main/java/cn/chriswood/anthill/infrastructure/web/annotation/log/StandardLogAppender.kt
+++ anthill-infrastructure/anthill-infrastructure-web-annotation/src/main/java/cn/chriswood/anthill/infrastructure/web/annotation/log/StandardLogAppender.kt
@@ -2,12 +2,14 @@ package cn.chriswood.anthill.infrastructure.web.annotation.log
 
 import org.aspectj.lang.JoinPoint
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.stereotype.Component
 
 /**
  * 默认实现的标准输出
  */
 @Component
+@ConditionalOnMissingBean(LogAppender::class)
 class StandardLogAppender : LogAppender {
 
     private val log = LoggerFactory.getLogger(javaClass)
