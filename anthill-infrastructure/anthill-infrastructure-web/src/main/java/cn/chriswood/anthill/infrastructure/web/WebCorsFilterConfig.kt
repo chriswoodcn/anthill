1650e001dbcc4d00bf71a22ba6f3072d1f07f098
chriswoodcn <chriswoodcn@aliyun.com>
2024-08-02 16:25:58 +0800

    [update] 使用WebCorsFilterConfig

---
+++ anthill-infrastructure/anthill-infrastructure-web/src/main/java/cn/chriswood/anthill/infrastructure/web/WebCorsFilterConfig.kt
@@ -0,0 +1,37 @@
+package cn.chriswood.anthill.infrastructure.web
+
+import cn.chriswood.anthill.infrastructure.json.JacksonConfig
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+
+@AutoConfiguration
+@AutoConfigureAfter(JacksonConfig::class)
+@ConditionalOnProperty(
+    prefix = "anthill.web.cors",
+    name = ["enabled"],
+    havingValue = "true",
+    matchIfMissing = true
+)
+class WebCorsFilterConfig {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration()
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        config.addAllowedOriginPattern("*")
+        config.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        log.debug(">>>>>>>>>> init WebCorsFilterConfig >>>>>>>>>>")
+        return CorsFilter(source)
+    }
+}
