dabcfb4f02e76f4138049758528a14be289c3246
chriswoodcn <chriswoodcn@aliyun.com>
2024-08-15 14:58:19 +0800

    [update] 后管模板分离出此项目

--- dashboard/api/admin/index.ts
+++
@@ -1,6 +0,0 @@
-export const login = async (data: any) =>
-  await fetch("/backend/login", {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
-  })
--- dashboard/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { stackMiddlewares } from '@/lib/middleware/stackMiddlewares';
-import { withRoot } from "@/lib/middleware/withRoot"
-import { withLogger } from "@/lib/middleware/withLogger"
-import { withAuth } from './lib/middleware/withAuth';
-
-const middlewares = [withLogger, withRoot, withAuth];
-
-export default stackMiddlewares(middlewares);
