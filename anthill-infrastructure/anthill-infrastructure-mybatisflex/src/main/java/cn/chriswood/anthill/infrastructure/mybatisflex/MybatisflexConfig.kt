e6857ff66e652109412a1020b0bf33359e9b4746
chriswoodcn <chriswoodcn@aliyun.com>
2025-01-03 13:30:29 +0800

    [update] MybatisflexConfig增加自定义主键生成器注册

--- anthill-infrastructure/anthill-infrastructure-mybatisflex/src/main/java/cn/chriswood/anthill/infrastructure/mybatisflex/MybatisflexConfig.kt
+++ anthill-infrastructure/anthill-infrastructure-mybatisflex/src/main/java/cn/chriswood/anthill/infrastructure/mybatisflex/MybatisflexConfig.kt
@@ -1,19 +1,29 @@
 package cn.chriswood.anthill.infrastructure.mybatisflex
 
-import cn.chriswood.anthill.infrastructure.mybatisflex.support.FlexInsertListener
-import cn.chriswood.anthill.infrastructure.mybatisflex.support.FlexUpdateListener
+import cn.chriswood.anthill.infrastructure.mybatisflex.support.*
 import com.mybatisflex.core.FlexGlobalConfig
 import com.mybatisflex.core.audit.AuditManager
 import com.mybatisflex.core.audit.AuditMessage
+import com.mybatisflex.core.keygen.KeyGeneratorFactory
 import com.mybatisflex.spring.boot.MyBatisFlexCustomizer
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.AutoConfiguration
+
 
 @AutoConfiguration
 class MybatisflexConfig : MyBatisFlexCustomizer {
 
     private val log = LoggerFactory.getLogger(javaClass)
     override fun customize(config: FlexGlobalConfig) {
+        KeyGeneratorFactory.register(
+            KeyGenerators.anthillUUID, GenKeyUUIDGenerator()
+        )
+        KeyGeneratorFactory.register(
+            KeyGenerators.anthillObjectId, GenKeyObjectGenerator()
+        )
+        KeyGeneratorFactory.register(
+            KeyGenerators.anthillSnowFlakeId, GenKeySnowflakeGenerator()
+        )
         // 全局配置insert监听
         config.registerInsertListener(FlexInsertListener())
         // 全局配置update监听
---
+++ anthill-infrastructure/anthill-infrastructure-mybatisflex/src/main/java/cn/chriswood/anthill/infrastructure/mybatisflex/support/KeyGenerators.kt
@@ -0,0 +1,11 @@
+package cn.chriswood.anthill.infrastructure.mybatisflex.support
+
+object KeyGenerators {
+    const val uuid = com.mybatisflex.core.keygen.KeyGenerators.uuid
+    const val flexId = com.mybatisflex.core.keygen.KeyGenerators.flexId
+    const val snowFlakeId = com.mybatisflex.core.keygen.KeyGenerators.snowFlakeId
+    const val ulid = com.mybatisflex.core.keygen.KeyGenerators.ulid
+    const val anthillSnowFlakeId = "anthillSnowFlakeId"
+    const val anthillUUID = "anthillUUID"
+    const val anthillObjectId = "anthillObjectId"
+}
